bf72706c9dfa43570bf5d2c7173c6b822223d3f4
Adnan Ademovic <adnanademovic100@gmail.com>
2016-10-16 20:17:23 +0200

    Remove log spam

--- scripts/gen_rs.py
+++ scripts/gen_rs.py
@@ -12,7 +12,6 @@ msg_template_map = { 'msg.rs.template':'@NAME@.rs' }
 srv_template_map = { 'srv.rs.template':'@NAME@.rs' }
 
 if __name__ == "__main__":
-    print sys.argv
     genmsg.template_tools.generate_from_command_line_options(sys.argv,
                                                              msg_template_map,
                                                              srv_template_map)
