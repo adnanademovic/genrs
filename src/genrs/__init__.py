9bb552c9d4608441b04d85ed24c995aacd343d6d
Adnan Ademovic <adnanademovic100@gmail.com>
2017-01-03 21:19:12 +0100

    Update messages to adhere to API

--- src/genrs/__init__.py
+++ src/genrs/__init__.py
@@ -1,9 +1,9 @@
 import genmsg.msgs
 
 try:
-    from cStringIO import StringIO #Python 2.x
+    from cStringIO import StringIO  #Python 2.x
 except ImportError:
-    from io import StringIO #Python 3.x
+    from io import StringIO  #Python 3.x
 
 MSG_TYPE_TO_RS = {
     'byte': 'i8',
@@ -19,10 +19,11 @@ MSG_TYPE_TO_RS = {
     'int64': 'i64',
     'float32': 'f32',
     'float64': 'f64',
-    'string': 'std::string::String',
+    'string': '::std::string::String',
     'time': 'u32',
     'duration': 'i32',
 }
+
 
 def depending_on(spec):
     """
@@ -34,14 +35,17 @@ def depending_on(spec):
             if (field.is_header):
                 packages.append('std_msgs')
             else:
-                (package, name) = genmsg.names.package_resource_name(field.base_type)
-                packages.append((package or spec.package) + ' ' + name) # convert '' to package
+                (package,
+                 name) = genmsg.names.package_resource_name(field.base_type)
+                packages.append((package or spec.package) + ' ' +
+                                name)  # convert '' to package
     return set(packages)
+
 
 def msg_type_to_rs(type, package):
     """
     Converts a message type (e.g. uint32, std_msgs/String, etc.) into the Rust declaration
-    for that type (e.g. u32, std_msgs.msg.String)
+    for that type (e.g. u32, std_msgs.String)
 
     @param type: The message type
     @type type: str
@@ -60,15 +64,16 @@ def msg_type_to_rs(type, package):
     else:
         pkg = base_type.split('/')[0]
         msg = base_type.split('/')[1]
-        rs_type = 'super::%s::%s'%(pkg, msg)
+        rs_type = 'super::%s::%s' % (pkg, msg)
 
     if is_array:
         if array_len is None:
-            return 'Vec<%s>'%(rs_type)
+            return 'Vec<%s>' % (rs_type)
         else:
-            return '(%s)'%(', '.join([rs_type for _ in range(array_len)]))
+            return '(%s)' % (', '.join([rs_type for _ in range(array_len)]))
     else:
         return rs_type
+
 
 def default_value(type, package):
     """
@@ -85,30 +90,33 @@ def default_value(type, package):
     """
     (base_type, is_array, array_len) = genmsg.msgs.parse_type(type)
     if is_array and array_len is None:
-        return 'vec![]'
+        return 'Vec::new()'
     rs_def = None
     rs_type = msg_type_to_rs(base_type, package)
-    if base_type in ['byte', 'int8', 'int16', 'int32', 'int64',
-                'char', 'uint8', 'uint16', 'uint32', 'uint64',
-                'time', 'duration']:
+    if base_type in [
+            'byte', 'int8', 'int16', 'int32', 'int64', 'char', 'uint8',
+            'uint16', 'uint32', 'uint64', 'time', 'duration'
+    ]:
         rs_def = '0' + rs_type
     elif base_type in ['float32', 'float64']:
         rs_def = '0.0' + rs_type
     elif base_type == 'bool':
         rs_def = 'false'
     elif base_type == 'string':
-        rs_def = 'std::string::String::new()'
+        rs_def = '::std::string::String::new()'
     else:
         rs_def = rs_type + '::new()'
     if is_array and array_len is not None:
-        return '(%s)'%(', '.join([rs_def for _ in range(array_len)]))
+        return '(%s)' % (', '.join([rs_def for _ in range(array_len)]))
     else:
         return rs_def
+
 
 def _escape_string(s):
     s = s.replace('\\', '\\\\')
     s = s.replace('"', '\\"')
     return s
+
 
 def escape_message_definition(definition):
     lines = definition.splitlines()
@@ -117,7 +125,7 @@ def escape_message_definition(definition):
     s = StringIO()
     for line in lines:
         line = _escape_string(line)
-        s.write('%s\\n\\\n'%(line))
+        s.write('%s\\n\\\n' % (line))
 
     val = s.getvalue()
     s.close()
